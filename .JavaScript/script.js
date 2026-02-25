a7d33646b8847e6dfc83d552b2ef626ee287a377
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-02-24 21:33:37 -0300

    dados
    testando tipos de dados

--- .JavaScript/script.js
+++ .JavaScript/script.js
@@ -1,2 +1,71 @@
 
-console.log("hello")
+/*
+  tipos de dados
+
+ strings --> textos
+ numbers --> numeros
+ boolean --> false and true
+ object -->  objetos
+ Null & undefined
+ Array -> Matriz
+
+ ""
+ ''
+ ´´
+
+ com "" e '' voce nao pode pular linha ja com crase ´´ voce pode pular linha a vontade
+*/
+
+const number = 47
+const whay = `whay swag ${number}`
+
+/*
+para fazer isso de colocar uma var dentro de outra precia ser com crase ``
+*/
+
+console.log(whay)
+
+const numero = 10 * 100 - (10+10)
+
+console.log(numero)
+
+/*
+OPERADORES ARITIMETICOS
+ para somar use "+"
+ para subitrair use "-"
+ para multiplicar use "*"
+ para dividir use "/"
+*/
+
+// const nome = "Andre"
+// const idade = 30
+// const casado = false
+
+const myArray = [
+ {
+    nome:'Andre',
+    idade: 28,
+    casado: true, 
+    conjuge: "andressa"
+},
+
+ {
+    nome:'Luiz',
+    idade: 30,
+    casado: false, 
+    conjuge: null
+}
+]
+
+console.log(myArray)
+
+/*Array */
+
+
+const usuarios = ["andre", "marcos", "guilherme"]
+
+console.log(usuarios[1])
+
+
+
+
