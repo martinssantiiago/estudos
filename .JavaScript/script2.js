4128df6c93432373e90bbe2df9bb876ef231e82e
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-02-24 23:33:50 -0300

    if and eslse
    comentarios sobre if else

--- .JavaScript/script2.js
+++ .JavaScript/script2.js
@@ -1,9 +1,20 @@
 /*IF and ELSE */
 
-const Nota = 5
-
-if(Nota > 5){
- console.log("Aluno APRAVADO")
-} else {
-    console.log("Aluno REPROVADO")
+const Nota = 6
+if(Nota >= 6){
+  console.log("aluno aprovado")
+}else{
+    console.log("aluno reprovado")
 }
+
+/*
+ operadores de comparaçao 
+  > / maior que 
+  < / menor que
+  == / igual que (ele compara somente o valor)
+  === / igual que (ele compara o TIPO e o VALOR)
+  >= / maior ou igual 
+  <= / menor ou igual
+*/
+
+ 
