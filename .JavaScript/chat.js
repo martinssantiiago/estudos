fd9e4559f8c9574072c649d7d6ab930846704c33
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-02-28 03:40:31 -0300

    estudos de javaScript
    funcoes, strings, number, variaveis(let, const)etc...

---
+++ .JavaScript/chat.js
@@ -0,0 +1,13 @@
+
+const a1 = "andre"
+const a2 = "24 anos"
+
+console.log(a1, a2)
+
+
+let n1 = 1 
+let n2 = 5
+
+console.log(n1 + n2)
+
+
---
+++ .JavaScript/js.html/script.js
@@ -0,0 +1,7 @@
+
+ const ValorReal = document.querySelector("input")
+
+     function converter() {
+        const valorConvertido = ValorReal.value / 5
+        alert(`o valor em dolar é ${valorConvertido} dolares`)
+     } 
