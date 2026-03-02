2e5e7a3262dbca03ae3deb62c498e6b1b051f4f4
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-02 04:52:32 -0300

    js-html-css
    descubra sua nacionalidade

--- .JavaScript/gustavoG/aula7.js
+++ .JavaScript/gustavoG/aula7.js
@@ -1,5 +1,11 @@
-let vel = 60.5
-
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-console.log(`digija com cuidado`)
-
+function ver() {
+    const localização = document.querySelector('#pais')
+    const res = document.querySelector('#res')   
+    const resu = (localização.value)
+      
+    if (localização.value != "Brasil") {
+        res.innerHTML = ('VOCE È ESTRANGEIRO')
+    } else {
+        res.innerHTML = ('VOCE È BRASILEIRO')
+    }
+} 
