19af673a1883643ac3f77df5a08d9e7b231a2ee9
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-06 07:53:12 -0300

    js

--- guanabaraG/exercicio1/script.js
+++ guanabaraG/exercicio1/script.js
@@ -1,12 +1,14 @@
 function conta() {
 
-let inicio = Number(document.getElementById(`inicio`).value)
-let fim = Number(document.getElementById(`fim`).value)
-let passos = Number(document.getElementById(`passos`).value)
+let inicio = (document.getElementById(`inicio`).value)
+let fim = (document.getElementById(`fim`).value)
+let passos = (document.getElementById(`passos`).value)
 let res = document.getElementById(`res`)
 
-if (inicio.value.length == 0 || fim.value.length == 0) {
-    alert(`oi`)
+if (inicio.length == 0 || fim.length == 0 || passos.length == 0 ) {
+    alert(`erro`)
+} else {
+    
 }
 
 }
