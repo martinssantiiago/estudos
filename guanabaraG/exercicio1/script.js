fe8332d74706bbcd3a566de766fd11696357f8dd
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-06 08:14:37 -0300

    exercicios

---
+++ guanabaraG/exercicio1/script.js
@@ -0,0 +1,12 @@
+function conta() {
+
+let inicio = Number(document.getElementById(`inicio`).value)
+let fim = Number(document.getElementById(`fim`).value)
+let passos = Number(document.getElementById(`passos`).value)
+let res = document.getElementById(`res`)
+
+if (inicio.value.length == 0 || fim.value.length == 0) {
+    alert(`oi`)
+}
+
+}
--- guanabaraG/repetiçoes/ambiente.js
+++ guanabaraG/repetiçoes/ambiente.js
@@ -1,5 +1,7 @@
 let c = 1
-while (c <= 5) {
-    console.log(c)
-    c++
-}
+ 
+ for (let c = 1;c <= 5; c++) {
+     console.log(c)
+ }
+
+ 
