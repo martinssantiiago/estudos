7032fbdce96bdfcfc6b1dfdf6f0d44e43ac45a09
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-06 07:58:57 -0300

    while

--- .JavaScript/gustavoG/ex003/cond.js
+++ .JavaScript/gustavoG/ex003/cond.js
@@ -2,10 +2,10 @@ function verificar() {
     const num = document.querySelector(`#numero`).value
     let res = document.querySelector(`#res`)
      
-     if(num < 10 ) {
-        res.innerHTML = `voce tem ${num} ano e é menor`
-     } if(num > 1) {
+     if(num < 18 ) {
         res.innerHTML = `voce tem ${num} anos e é menor`
+     } if(num >= 18) {
+        res.innerHTML = `voce tem ${num} anos e é maior `
      } if(num === ``) {
         alert(`coloque sua idade`)
      }
