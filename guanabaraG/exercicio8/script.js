8f866b89df550fdcea7ac2b5c9a35666e3cd44d4
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-18 07:39:39 -0300

    js
    calcucladore de IMC

---
+++ guanabaraG/exercicio8/script.js
@@ -0,0 +1,25 @@
+function calcular() {
+  let nome = document.getElementById(`nome`).value;
+  let altura = Number(document.getElementById(`altura`).value);
+  let peso = Number(document.getElementById(`peso`).value);
+
+  let res = document.getElementById(`res`);
+
+  let nm = nome;
+
+  let classificacao = ``
+
+  let imc = peso / (altura * altura);
+
+  if(imc < 18.5) {
+    classificacao = `abaixo do peso`
+  } else if (imc < 25) {
+    classificacao = `com peso normal`
+  } else if (imc < 30) {
+    classificacao = `com sobre peso`
+  } else {
+    classificacao = `com obesidade`
+  }
+
+  res.innerHTML = `${nm}, seu IMC é ${imc.toFixed(2)}, e voce esta${classificacao}`;
+}
