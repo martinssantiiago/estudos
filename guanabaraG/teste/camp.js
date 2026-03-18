3afdf717d0a56fd778db2de8b69fc0de56a26b45
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-18 07:09:43 -0300

    delete

--- guanabaraG/teste/camp.js
+++ guanabaraG/teste/camp.js
@@ -1,18 +1,2 @@
-let nome = document.getElementById(`nome`)
-let resp = document.querySelector(`.res`)
+    
 
-let nomes = []
-
-
-function ver(){
-   let input = nome.value
-   
-   if( nome.value === ``) {
-    alert(`digite seu nome`)
-    return
-   }
-
-   nomes.push(input)
-
-   resp.innerHTML = nomes
-}
